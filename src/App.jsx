ad24f422bea2b6b100dccbe121a71e7fc4c9bd7d
Evandabest <evanhaque1@gmail.com>
2024-10-24 01:52:01 -0400

    added backup code for usestate and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import React from 'react';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col items-center w-full h-full">
+      <p className="text-2xl font-bold">Hello World!</p>
+      
+    </div>
+  );
 }
 
-export default App
+export default App; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import Counter from './assets/components/counter';
+
+//Usestate example
+//import { useState } from "react"
+//
+//function App() {
+//  
+//  const [count, setCount] = useState(0)
+//
+//  return (
+//    <>
+//    <div className="flex flex-col items-center w-full h-full">
+//      <p className="mb-10 font-bold ">Count: {count}</p>
+//      <div className="flex flex-row gap-5">
+//      <button onClick={() => {setCount(count+1)}}>Increment</button>
+//      <button onClick={() => {setCount(count-1)}}>Decrement</button>
+//      </div>
+//    </div>
+//      
+//    </>
+//  )
+//}
+//
+//export default App
+
+
+
+//https://pokeapi.co/api/v2/pokemon
+//import { useState, useEffect } from 'react';
+//
+//function App() {
+//  const [name, setName] = useState(null);
+//  const [picture, setPicture] = useState(null);
+//
+//  useEffect(() => {
+//    const getPokemon = async () => {
+//      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+//      const data = await response.json()
+//      console.log(data)
+//      console.log(data.sprites.front_default)
+//      setName(data.name)
+//      setPicture(data.sprites.front_default)
+//    }
+//    getPokemon()
+//  }, []); 
+//
+//  return (
+//    <div className='flex flex-col items-center'>
+//    <p>{name}</p>
+//    <img src={picture} alt="pikachu" />
+//    </div>
+//  )
+//}
+//
+//export default App;
